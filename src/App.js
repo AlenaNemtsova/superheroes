ed21388822f9700c0f17b9ea5df56dda04bd70ad
Alena Nemtsova <jelenatbs@gmail.com>
2022-04-24 23:20:56 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Superhero from './components/superhero';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Superhero url="./assets/img/batman.jpg" name="Бэтмен" alter="Брюс Уэйн" power="интеллект, обширные познания, знания боевых искусств, ловкость"></Superhero>
+      <Superhero url="./assets/img/superman.jpg" name="Супермен" alter="Кларк Кент" power="непробиваемость, суперсила, полет, самоисцеление, суперзрение и суперслух, классный костюм"></Superhero>
+      <Superhero url="./assets/img/iron-man.jpg" name="Железный человек" alter="Тони Старк" power="высокий уровень интеллекта, широкие познания науки и техники, связь со всемирной паутиной, бронекостюмы"></Superhero>
+      <Superhero url="./assets/img/spider-man.jpg" name="Человек-паук" alter="Питер Паркер" power="сверхчеловеческие рефлексы, «паучье чутье», способность прилепляться к твердым поверхностям, производство паутины"></Superhero>
+      <Superhero url="./assets/img/captain-america.jpg" name="Капитан Америка" alter="Стивен Роджерс" power="сила, выносливость, бессмертие, быстрая регенерация, мастерство скрытности и боя"></Superhero>
+      <Superhero url="./assets/img/thor.jpg" name="Тор" alter="нет; полное имя — Тор Одинсон" power="все, что может бог, плюс молот Мьелнир"></Superhero>
+      <Superhero url="./assets/img/halk.jpg" name="Халк" alter="Брюс Беннер" power="сверхчеловеческая сила искорость, выносливость, полет"></Superhero>
+      <Superhero url="./assets/img/wonder-woman.jpg" name="Чудо-женщина" alter="Диана Принс" power="сверхчеловеческая сила искорость, выносливость, полет"></Superhero>
     </div>
   );
 }
---
+++ src/components/superhero.jsx
@@ -0,0 +1,21 @@
+import './superhero.css'
+
+
+function Superhero(props) {
+    return(
+
+<div className="superhero-card">
+    <img src={props.url} alt={props.name}></img>
+    
+        <h2 className="superhero-name">{props.name}</h2>
+        <div className="superhero-card__text">
+        <p className="superhero-alter">Альтер эго: {props.alter}</p>
+        <p className="superhero-power">Суперсилы: {props.power}</p>
+        </div>
+
+    </div>
+    );
+
+}
+
+export default Superhero;
