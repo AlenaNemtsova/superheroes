804743f5ff7e8493ef5b48a5101b93b010adf3d2
Alena Nemtsova <jelenatbs@gmail.com>
2022-04-26 21:00:52 +0300

    changed to a method map

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import './App.css';
 import Superhero from './components/superhero';
 
+const superheroes = [
+  { url: "./assets/img/batman.jpg", name: "Бэтмен", alter: "Брюс Уэйн", power: "интеллект, обширные познания, знания боевых искусств, ловкость" },
+  { url: "./assets/img/superman.jpg", name: "Супермен", alter: "Кларк Кент", power: "непробиваемость, суперсила, полет, самоисцеление, суперзрение и суперслух, классный костюм" },
+  { url: "./assets/img/iron-man.jpg", name: "Железный человек", alter: "Тони Старк", power: "высокий уровень интеллекта, широкие познания науки и техники, связь со всемирной паутиной, бронекостюмы" },
+  { url: "./assets/img/spider-man.jpg", name: "Человек-паук", alter: "Питер Паркер", power: "сверхчеловеческие рефлексы, «паучье чутье», способность прилепляться к твердым поверхностям, производство паутины" },
+  { url: "./assets/img/captain-america.jpg", name: "Капитан Америка", alter: "Стивен Роджерс", power: "сила, выносливость, бессмертие, быстрая регенерация, мастерство скрытности и боя" },
+  { url: "./assets/img/thor.jpg", name: "Тор", alter: "нет; полное имя — Тор Одинсон", power: "все, что может бог, плюс молот Мьелнир" },
+  { url: "./assets/img/halk.jpg", name: "Халк", alter: "Брюс Беннер", power: "сверхчеловеческая сила искорость, выносливость, полет" },
+  { url: "./assets/img/wonder-woman.jpg", name: "Чудо-женщина", alter: "Диана Принс", power: "сверхчеловеческая сила искорость, выносливость, полет" }
+];
+
+
 function App() {
   return (
     <div className="App">
-      <Superhero url="./assets/img/batman.jpg" name="Бэтмен" alter="Брюс Уэйн" power="интеллект, обширные познания, знания боевых искусств, ловкость"></Superhero>
-      <Superhero url="./assets/img/superman.jpg" name="Супермен" alter="Кларк Кент" power="непробиваемость, суперсила, полет, самоисцеление, суперзрение и суперслух, классный костюм"></Superhero>
-      <Superhero url="./assets/img/iron-man.jpg" name="Железный человек" alter="Тони Старк" power="высокий уровень интеллекта, широкие познания науки и техники, связь со всемирной паутиной, бронекостюмы"></Superhero>
-      <Superhero url="./assets/img/spider-man.jpg" name="Человек-паук" alter="Питер Паркер" power="сверхчеловеческие рефлексы, «паучье чутье», способность прилепляться к твердым поверхностям, производство паутины"></Superhero>
-      <Superhero url="./assets/img/captain-america.jpg" name="Капитан Америка" alter="Стивен Роджерс" power="сила, выносливость, бессмертие, быстрая регенерация, мастерство скрытности и боя"></Superhero>
-      <Superhero url="./assets/img/thor.jpg" name="Тор" alter="нет; полное имя — Тор Одинсон" power="все, что может бог, плюс молот Мьелнир"></Superhero>
-      <Superhero url="./assets/img/halk.jpg" name="Халк" alter="Брюс Беннер" power="сверхчеловеческая сила искорость, выносливость, полет"></Superhero>
-      <Superhero url="./assets/img/wonder-woman.jpg" name="Чудо-женщина" alter="Диана Принс" power="сверхчеловеческая сила искорость, выносливость, полет"></Superhero>
+      {
+        superheroes.map((superhero) => <Superhero url={superhero.url} name={superhero.name} alter={superhero.alter} power={superhero.power}></Superhero>
+        )
+      }
     </div>
   );
 }
--- src/components/superhero.jsx
+++ src/components/superhero.jsx
@@ -2,20 +2,17 @@ import './superhero.css'
 
 
 function Superhero(props) {
-    return(
+    return (
 
-<div className="superhero-card">
-    <img src={props.url} alt={props.name}></img>
-    
-        <h2 className="superhero-name">{props.name}</h2>
-        <div className="superhero-card__text">
-        <p className="superhero-alter">Альтер эго: {props.alter}</p>
-        <p className="superhero-power">Суперсилы: {props.power}</p>
+        <div className="superhero-card">
+            <img src={props.url} alt={props.name}></img>
+            <h2 className="superhero-name">{props.name}</h2>
+            <div className="superhero-card__text">
+                <p className="superhero-alter">Альтер эго: {props.alter}</p>
+                <p className="superhero-power">Суперсилы: {props.power}</p>
+            </div>
         </div>
-
-    </div>
     );
-
 }
 
 export default Superhero;
